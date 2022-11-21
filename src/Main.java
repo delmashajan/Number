40ac08a2ba458069add3f2069aa144de5b6e08ef
delmashajan <delmashajan@gmail.com>
2022-11-21 15:53:03 +0530

    switch added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
 
     public static double largest(double a, double b, double c ){
@@ -53,7 +54,80 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("Check Number!");
+        int flag =0;
+        while(flag ==0){
+            System.out.println("1.Find largest among three numbers\n"+
+                    "2.Find smallest among three numbers\n"+
+                    "3.Check prime or not\n"+
+                    "4.Check even or not\n"+
+                    "5.Reverse the number\n");
+            System.out.println("Enter your choice");
+
+
+
+            Scanner input = new Scanner(System.in);
+            int choice = input.nextInt();
+            switch (choice){
+                case 1:
+                    System.out.println("Enter first no");
+                    double a = input.nextInt();
+                    System.out.println("Enter second no");
+                    double b = input.nextInt();
+                    System.out.println("Enter third no");
+                    double c = input.nextInt();
+                    double large = largest(a,b,c);
+                    System.out.println(large);
+                    break;
+                case 2:
+                    System.out.println("Enter first no");
+                    double j = input.nextInt();
+                    System.out.println("Enter second no");
+                    double k = input.nextInt();
+                    System.out.println("Enter third no");
+                    double l = input.nextInt();
+                    double small = smallest(j,k,l);
+                    System.out.println(small);
+                    break;
+                case 3:
+                    System.out.println("Enter no to check prime or not");
+                    int no = input.nextInt();
+                    primeOrNot(no);
+                    break;
+                case 4:
+                    System.out.println("Enter no to check even or not");
+                    int cho = input.nextInt();
+                    evenOrNot(cho);
+                    break;
+                case 5:
+                    System.out.println("Enter no to be reversed");
+                    int choi = input.nextInt();
+                    reverse(choi);
+                    break;
+
+
+
+                default:
+                    System.out.println("invalid input");
+            }
+            System.out.println("Do you want to repeat the process if yes enter 1 else 0");
+            int option2 = input.nextInt();
+            if(option2 == 1){
+                flag = 0;
+            }
+            else{
+                flag = 1;
+                System.out.println("Happy computing bbye");
+            }
+
+
+
+
+        }
+
+
+
 
     }
-}
+
+
+    }
